18275633c25e4623e6b15f08709cd8bd12193481
Dan Molina <32586421+quietcomet777@users.noreply.github.com>
2018-09-10 22:27:08 -0700

    Add files via upload
    part 3 of upload from Unity cloud build

---
+++ Assets/blocklife.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocklife : MonoBehaviour {
+
+	float minDist;
+	GameObject[] ships;
+
+	void Start(){
+		StartCoroutine(Check());
+	}
+
+	IEnumerator Check(){
+		yield return new WaitForSeconds (10);	//start within 10 s
+		while (true) {
+			minDist = 20;
+			foreach (GameObject ship in GameObject.FindGameObjectsWithTag("Ship")) {
+				float thisDist = (ship.transform.position - transform.position).magnitude;
+				if (thisDist < minDist) {
+					minDist = thisDist;
+				}
+			}
+			if (minDist > 15) {
+				Destroy (gameObject);
+			}
+			yield return new WaitForSeconds (3);
+		}
+	}
+}
